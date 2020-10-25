a66cc47975cf9a735a5321526b38fb7344f1e11c
amevada9 <amevada9@users.noreply.github.com>
2020-10-24 23:22:36 -0400

    Apache settings changed.

--- voter_site/wsgi.py
+++ voter_site/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voter_site.settings')
